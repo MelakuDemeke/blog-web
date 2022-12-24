bcdc4d5518aa8fe3d5cfc2ced1a406a6e8774430
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-24 11:16:48 +0300

    contact and about added

--- app.js
+++ app.js
@@ -16,9 +16,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/', function(req,res){
-  res.render('home',{homeStartingContent:homeStartingContent});
+	res.render('home',{homeStartingContent:homeStartingContent});
 });
 
+app.get('/about',function(req,res){
+	res.render('about',{aboutContent:aboutContent});
+});
+
+app.get('/contact',function (req,res) {
+	res.render('contact',{contactContent:contactContent});
+});
 
 
 
