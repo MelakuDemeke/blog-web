e3d00fc97ca15958c50c4d1cb7404496b3ae4d72
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-25 10:34:51 +0300

    var type changed

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
+let posts = [];
 
 
 app.get('/', function(req,res){
